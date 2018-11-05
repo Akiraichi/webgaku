9e226d7c83a66ee327992c7ad37acfbc2392f771
Nakayama <akiraichi5430@gmail.com>
2018-11-05 16:28:27 +0900

    マイグレーションファイルの作成

---
+++ db/migrate/20181105072356_create_members.rb
@@ -0,0 +1,15 @@
+class CreateMembers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :members do |t|
+      t.string :name
+      t.string :yomi
+      t.string :comment
+      t.string :position
+      t.string :facebook
+      t.string :twitter
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
