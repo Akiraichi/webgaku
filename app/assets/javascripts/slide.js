1e17917d8c74f0479768e121e57657b2fab849fe
Nakayama <akiraichi5430@gmail.com>
2018-11-07 10:26:20 +0900

    さらに全体的にそれっぽくした

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -1,6 +1,17 @@
 $(function () {
   $('#slidelink').on('click', function (e) {
     e.preventDefault();
-    location.href = "http://localhost:3000/about";
+    location.href = "http://localhost:3000/gallery/1";
+  });
+
+  $('#slidelink').on('click', function (e) {
+    e.preventDefault();
+    location.href = "http://localhost:3000/gallery/2";
+  });
+
+  $('#slidelink').on('click', function (e) {
+    e.preventDefault();
+    location.href = "http://localhost:3000/gallery/3";
   });
 })
+
--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -2,4 +2,8 @@ class GalleryController < ApplicationController
   def index
     render 'gallery'
   end
+  def show
+    id = params[:id]
+    render "#{id}"
+  end
 end
