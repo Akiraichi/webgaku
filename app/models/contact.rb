2c97767474dac3627938b586354a4d7b348444d6
Nakayama <akiraichi5430@gmail.com>
2018-11-08 14:47:40 +0900

    dwerw

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,7 +7,6 @@ class Contact < ApplicationRecord
   # validates_numericality_of :phone, { :message => '電話番号は数字で入力して下さい。'}
   validates :message, :presence => { :message => '問い合わせ内容を入力して下さい。'}
   after_create do
-    cct=Contact.last
-    $ttx=cct
+    $ttx=Contact.last.message
   end
 end
