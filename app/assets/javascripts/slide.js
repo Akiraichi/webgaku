1f08a71cd222fc0e7537e8acce2f0dfed019a3e2
Nakayama <akiraichi5430@gmail.com>
2018-11-08 10:22:07 +0900

    リンクを修正

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -1,17 +1,17 @@
 $(function () {
   $('#slidelink1').on('click', function (e) {
     e.preventDefault();
-    location.href = "http://localhost:3000/gallery/1";
+    location.href = "/gallery/1";
   });
 
   $('#slidelink2').on('click', function (e) {
     e.preventDefault();
-    location.href = "http://localhost:3000/gallery/2";
+    location.href = "/gallery/2";
   });
 
   $('#slidelink3').on('click', function (e) {
     e.preventDefault();
-    location.href = "http://localhost:3000/gallery/3";
+    location.href = "/gallery/3";
   });
 })
 
