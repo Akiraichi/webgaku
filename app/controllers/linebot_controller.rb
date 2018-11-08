4a7a925dcfd565e37014333c0b6c61d0114e215e
Nakayama <akiraichi5430@gmail.com>
2018-11-09 01:16:32 +0900

    aa

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -28,7 +28,7 @@ class LinebotController < ApplicationController
 
     events = client.parse_events_from(body)
     events.each { |event|
-      puts event["message"]
+      puts event["source"]
       case event
       when Line::Bot::Event::Message
         case event.type
