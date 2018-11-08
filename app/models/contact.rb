03949214484bdef35d4b7d867dfcf7264051cb3f
Nakayama <akiraichi5430@gmail.com>
2018-11-08 14:52:01 +0900

    aaa

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   # attr_accessor :name, :email, :phone, :message
-
+require 'line/bot' 
   validates :name,   length: { minimum: 3, :too_short => '名前を入力して下さい。'}
   validates :email,  length: { minimum: 3, :too_short => 'メールアドレスを入力して下さい。'}
   validates :phone,  length: { minimum: 3, :too_short => 'メールアドレスを入力して下さい。'}
@@ -8,5 +8,6 @@ class Contact < ApplicationRecord
   validates :message, :presence => { :message => '問い合わせ内容を入力して下さい。'}
   after_create do
     $ttx=Contact.last.message
+    client.push_message(ENV["PUSH_TO_ID"], text_message($ttx))
   end
 end
