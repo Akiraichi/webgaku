7e6eddecb940f6746cd46e3b1f26c86e2ced7db2
Nakayama <akiraichi5430@gmail.com>
2018-11-09 00:50:50 +0900

    ss

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -45,6 +45,8 @@ class LinebotController < ApplicationController
             elsif text == "今の気温は？"
               message = env_sensor
             end
+            puts event[:source]
+            puts event.source
             message = event.source["userId"]
           client.reply_message(event['replyToken'], text_message(message))
         end
