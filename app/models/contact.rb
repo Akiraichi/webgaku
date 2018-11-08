43e883ec044629b8f09e9f8f24be51d5d16d10f3
Nakayama <akiraichi5430@gmail.com>
2018-11-08 14:56:01 +0900

    aa

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,19 @@ require 'line/bot'
   validates :phone,  length: { minimum: 3, :too_short => 'メールアドレスを入力して下さい。'}
   # validates_numericality_of :phone, { :message => '電話番号は数字で入力して下さい。'}
   validates :message, :presence => { :message => '問い合わせ内容を入力して下さい。'}
+  
+  def text_message(text)
+    {
+        "type" => "text",
+        "text" => text
+    }
+  end
+  
   after_create do
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
     $ttx=Contact.last.message
     client.push_message(ENV["PUSH_TO_ID"], text_message($ttx))
   end
